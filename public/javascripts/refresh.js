e11cf650f132f7d4d4d511e49d470814e1019007
Kevin Chiu <kevin@kevinchiu.org>
2010-03-28 08:06:35 -0400

    faster refresh + iframe fixes

--- public/javascripts/refresh.js
+++ public/javascripts/refresh.js
@@ -11,8 +11,8 @@ function jsonFlickrApi(o) {
 	}else{
 		current = o.toSource()
 		if (current != baseline) {
-			rand_30 = Math.floor(Math.random()*31)
-			setTimeout("window.location=window.location", (rand_30+10)*1000)
+			rand_5 = Math.floor(Math.random()*6)
+			setTimeout("window.location=window.location", rand_5*1000)
 		}		
 	}
 }
@@ -20,4 +20,4 @@ function jsonFlickrApi(o) {
 first = true
 url = "http://www.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=739e578b0095d2ce5978331ac7466bd4&tags=iccp10,iccp2010&per_page=30"
 
-setInterval("checkFreshness();", 600000)
+setInterval("checkFreshness();", 100000)
