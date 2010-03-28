3dad64a2a7d38b5f7df9b9573e5462d60574dfbf
Kevin Chiu <kevin@kevinchiu.org>
2010-03-28 15:19:32 -0400

    shuffle

--- app/controllers/refresh_controller.rb
+++ app/controllers/refresh_controller.rb
@@ -20,17 +20,13 @@ class RefreshController < ApplicationController
   end
   
   def photo_hash_array
-    flickr_search.map{|p| photo_to_hash(p)}.to_json
+    flickr_search.shuffle.map{|p| photo_to_hash(p)}.to_json
   end
   
   def flickr_search
-    tags = params[:tags]
     per_page = params[:per_page]
-    # tags = "iccp2010,iccp10"
-    # per_page = "5"
     require 'xmlsimple'
-    url = "http://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=739e578b0095d2ce5978331ac7466bd4&user_id=48727269@N07&extras=url_o,url_s,url_m,url_l,date_upload"
-    # url = "http://www.flickr.com/services/rest/?method=flickr.photos.search&format=rest&api_key=739e578b0095d2ce5978331ac7466bd4&tags=#{tags}&per_page=#{per_page}&extras=url_o,url_s,url_m,url_l,date_upload"
+    url = "http://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=739e578b0095d2ce5978331ac7466bd4&user_id=48727269@N07&extras=url_o,url_s,url_m,url_l,date_upload&per_page=#{per_page}"
     xml_data = Net::HTTP.get_response(URI.parse(url)).body
     data = XmlSimple.xml_in(xml_data)
     data['photos'][0]['photo'].sort{|x,y| y["dateupload"].to_i <=> x["dateupload"].to_i}
