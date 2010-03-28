ea2b3045e686999bbb5fd959ac9d6a1adceb7095
Kevin Chiu <kevin@kevinchiu.org>
2010-03-28 09:16:13 -0400

    fixed error in dateupload logic

--- app/controllers/refresh_controller.rb
+++ app/controllers/refresh_controller.rb
@@ -24,12 +24,14 @@ class RefreshController < ApplicationController
   end
   
   def flickr_search
-    tags = params[:tags]
-    per_page = params[:per_page]
+    # tags = params[:tags]
+    # per_page = params[:per_page]
+    tags = "iccp2010,iccp10"
+    per_page = "5"
     require 'xmlsimple'
     url = "http://www.flickr.com/services/rest/?method=flickr.photos.search&format=rest&api_key=739e578b0095d2ce5978331ac7466bd4&tags=#{tags}&per_page=#{per_page}&extras=url_o,url_s,url_m,date_upload"
     xml_data = Net::HTTP.get_response(URI.parse(url)).body
     data = XmlSimple.xml_in(xml_data)
-    data['photos'][0]['photo'].sort{|x,y| y["dateupload".to_i] <=> x["dateupload"].to_i}
+    data['photos'][0]['photo'].sort{|x,y| y["dateupload"].to_i <=> x["dateupload"].to_i}
   end
 end
