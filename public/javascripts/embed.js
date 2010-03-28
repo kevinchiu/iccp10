d44c2da945498070934990b86abb7161f7c55fa9
Kevin Chiu <kevin@kevinchiu.org>
2010-03-28 08:20:38 -0400

    embed

--- public/javascripts/embed.js
+++ public/javascripts/embed.js
@@ -1,5 +1,9 @@
-embed = document.createElement('iframe')
-embed.setAttribute('width', '100%')
-embed.setAttribute('height', '100%')
-embed.setAttribute('frameborder', '0')
-embed.setAttribute('src', 'http://iccp10.com')
+function embedSlideshow(){
+	embed = document.createElement('iframe')
+	embed.setAttribute('width', '100%')
+	embed.setAttribute('height', '100%')
+	embed.setAttribute('frameborder', '0')
+	embed.setAttribute('src', 'http://iccp10.com')
+}
+
+window.onload = embedSlideshow()
