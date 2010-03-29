afe61ad0400f6d4c8e8876c7888d7bd08a2045e6
Kevin Chiu <kevin@kevinchiu.org>
2010-03-29 15:20:29 -0400

    refresh index fixed

--- public/javascripts/refresh.js
+++ public/javascripts/refresh.js
@@ -1,19 +1,19 @@
 function checkFreshness () {
 	url = "http://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=739e578b0095d2ce5978331ac7466bd4&user_id=48727269@N07&format=json&extras=url_o,url_s,url_m,url_l,date_upload"
 	results = new JSONscriptRequest(url)
-	results.buildScriptTag(); 
+	results.buildScriptTag();
 	results.addScriptTag();
 }
 
 function jsonFlickrApi(o) {
-	current = o.photos.photo[photo.length-1].dateupload
+	current = o.photos.photo[o.photos.photo.length-1].dateupload
 	if(first){
 		baseline = current
 		first = false
 	}else{
 		if (current != baseline) {
 			window.location=window.location
-		}		
+		}
 	}
 }
 
