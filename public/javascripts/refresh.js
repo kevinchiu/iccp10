9fdd343d21296d1b6e0fa16ed3f75ce0c842fed0
Kevin Chiu <kevin@kevinchiu.org>
2010-03-29 21:18:16 -0400

    zeroed index for rereshing

--- public/javascripts/refresh.js
+++ public/javascripts/refresh.js
@@ -6,7 +6,7 @@ function checkFreshness () {
 }
 
 function jsonFlickrApi(o) {
-	current = o.photos.photo[o.photos.photo.length-1].dateupload
+	current = o.photos.photo[0].dateupload
 	if(first){
 		baseline = current
 		first = false
